8105b5cc95d5f9879cdc58d3ba7a6a468b3d8e62
itzm1ta <87873333+itzm1ta@users.noreply.github.com>
2023-05-06 13:25:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let buton1 = document.querySelector('.btn1') //Наше меню
+let buton2 = document.querySelector('.btn2') //Кафе "ШАУРМУ Х@ЧУ
+let buton3 = document.querySelector('.btn3')//Закажи доставку
+let buton4 = document.querySelector('.btn4')//Наши новости
+let buton5 = document.querySelector('.btn5')//О "ШАУРМУ Х@ЧУ"
+let buton6 = document.querySelector('.btn6')//Свяжитесь с нами
+
+
+document.getElementById('button1').onclick = function(){
+    window.location.href='hshaurmu-hachu.ru';
+};
+
+
+
+// padding внутренний отступ
+// margin внешний отступ
